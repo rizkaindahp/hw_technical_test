52490065bd98d947e4520aec54ae06dc926f9f3e
Rizka Indah Puspita <rWX1073915@china.huawei.com>
2024-02-17 10:50:59 +0700

    set register

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Get the form element
   var form = document.getElementById("loginForm");
+  var register = document.getElementById("registForm");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -24,7 +25,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // Check if password meets requirements
     var password = document.getElementById("password").value;
     if (password.length < 8) {
-      alert("Password must be at least 8 characters long.");
+      swal({
+        title: "warning!",
+        text: "Password must be at least 8 characters long.",
+        icon: "warning",
+        timer: 1000, // 3000 milliseconds (3 seconds)
+        buttons: false,
+        showCloseButton: false,
+        timerProgressBar: true, // Enable progress bar
+      });
       event.preventDefault();
       return;
     }
@@ -34,15 +43,113 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log(email, password);
   });
 
+  register.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Check if name is valid
+    var name = document.getElementById("regist_name").value;
+    if (!validateName(name)) {
+      swal({
+        title: "warning!",
+        text: "Please enter a valid name",
+        icon: "warning",
+        timer: 1000, // 3000 milliseconds (3 seconds)
+        buttons: false,
+        showCloseButton: false,
+        timerProgressBar: true, // Enable progress bar
+      });
+      event.preventDefault();
+      return;
+    }
+
+    // Check if email is valid
+    var email = document.getElementById("regist_email").value;
+    if (!validateEmail(email)) {
+      swal({
+        title: "warning!",
+        text: "Please enter a valid email address",
+        icon: "warning",
+        timer: 1000, // 3000 milliseconds (3 seconds)
+        buttons: false,
+        showCloseButton: false,
+        timerProgressBar: true, // Enable progress bar
+      });
+      event.preventDefault();
+      return;
+    }
+
+    // Check if phone is valid
+    var phone = document.getElementById("regist_phone").value;
+    if (!validatePhone(phone)) {
+      swal({
+        title: "warning!",
+        text: "Please enter a valid phone number",
+        icon: "warning",
+        timer: 1000, // 3000 milliseconds (3 seconds)
+        buttons: false,
+        showCloseButton: false,
+        timerProgressBar: true, // Enable progress bar
+      });
+      event.preventDefault();
+      return;
+    }
+
+    // Check if password meets requirements
+    var password = document.getElementById("regist_password").value;
+    if (password.length < 8) {
+      swal({
+        title: "warning!",
+        text: "Password must be at least 8 characters long.",
+        icon: "warning",
+        timer: 1000, // 3000 milliseconds (3 seconds)
+        buttons: false,
+        showCloseButton: false,
+        timerProgressBar: true, // Enable progress bar
+      });
+      event.preventDefault();
+      return;
+    }
+
+    regist(name, email, phone, password);
+
+    console.log(name, email, phone, password);
+  });
+
   function validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
+  }
+
+  function validateName(name) {
+    var re = /^[a-zA-Z ]+$/;
+    return re.test(name);
+  }
+
+  function validatePhone(phone) {
+    var re = /^\d+$/;
+    return re.test(phone);
   }
 
   async function login(email, password) {
     try {
       const response = await axios.post("http://localhost:3000/login", {
         email: email,
+        password: password,
+      });
+      // Handle success
+      console.log(response.data);
+    } catch (error) {
+      // Handle error
+      console.error(error);
+      alert("Error occurred. Please check console for details.");
+    }
+  }
+
+  async function regist(name, email, phone, password) {
+    try {
+      const response = await axios.post("http://localhost:3000/register", {
+        name: name,
+        email: email,
+        phone: phone,
         password: password,
       });
       // Handle success
--- server/index.js
+++ server/index.js
@@ -1,67 +1,94 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://rizka96:rizka96@cluster0.trvnxb1.mongodb.net/hw_test?retryWrites=true&w=majority')
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(
+    "mongodb+srv://rizka96:rizka96@cluster0.trvnxb1.mongodb.net/hw_test?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-
-  const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-  });
-
-  const User = mongoose.model('User', userSchema);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  password: String
 });
 
-app.post('/login', async (req, res) => {
-    // Assuming the client sends email and password in the request body
-    const { email, password } = req.body;
-    console.log(email, password)
-  
-    try {
-        // Create a new user instance
-        const newUser = new User({
-          email: email,
-          password: password
-        });
+const User = mongoose.model("User", userSchema);
 
-        // Save the user to the database
-        await newUser.save();
-
-        const response = {
-          success: true,
-          msg: 'User registered successfully'
-        };
-        res.json(response);
-    } catch (error) {
-        console.error('Error registering user:', error);
-        const response = {
-          success: false,
-          msg: 'Failed to register user'
-        };
-        res.status(500).json(response);
-    }
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-  app.get('/register', (req, res) => {
+app.post("/login", async (req, res) => {
+  // Assuming the client sends email and password in the request body
+  const { email, password } = req.body;
+  console.log(email, password);
+
+  try {
+    // Create a new user instance
+    const loginUser = new User({
+      email: email,
+      password: password,
+    });
+
+    // Save the user to the database
+    await loginUser.save();
+
     const response = {
-        success: true,
-        msg: 'register'
-    }
+      success: true,
+      msg: "User login successfully",
+    };
     res.json(response);
-  });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    const response = {
+      success: false,
+      msg: "Failed to register user",
+    };
+    res.status(500).json(response);
+  }
+});
+
+app.get("/register", async (req, res) => {
+  // Assuming the client sends email and password in the request body
+  const { name, email, phone, password } = req.body;
+  console.log(name, email, phone, password,);
+
+  try {
+    // Create a new user instance
+    const newUser = new User({
+      name: name,
+      email: email,
+      phone: phone,
+      password: password,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    const response = {
+      success: true,
+      msg: "User registered successfully",
+    };
+    res.json(response);
+  } catch (error) {
+    console.error("Error registering user:", error);
+    const response = {
+      success: false,
+      msg: "Failed to register user",
+    };
+    res.status(500).json(response);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
